033f0d656b0a9f7832a162789d65c0d5eda3d0f1
unknown <furqannadeem410@gmail.com>
2025-12-02 03:32:51 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+// Assignment 10 
+// Q no: 01
+
+// function showDate () {
+//     let currentDate = new Date();
+//     document.write(currentDate);
+// }
+
+// showDate();
+
+// Q no: 02
+
+// function userName (firstName , lastName) {
+// let fullName = (firstName  + " " + lastName);
+// alert(fullName);
+// console.log(userName);
+// }
+
+// userName("Furqan" ,"Nadeem");
+
+// Q no: 03
+
+// function addNumbers() {
+//     let num1 = +prompt("Entre your first number");
+//     let num2 = +prompt("Entre your seconde number");
+//     return num1 + num2;
+// }
+
+
+// let sum = addNumbers();
+// alert("sum is " + sum);
+// document.write(sum);
+
+// // Q no: 04
+
+// function calculator(num1, num2, operator) {
+//     if (operator === "+") return num1 + num2;
+//     if (operator === "-") return num1 - num2;
+//     if (operator === "*") return num1 * num2;
+//     if (operator === "/") return num1 / num2;
+//     return "Invalid operator";
+// }
+// document.write("Result: " + calculator(10, 5, "*"));
+
+// Q no: 05
+
+// function factorial(n) {
+//     let fact = 1;
+//     for (let i = 1; i <= n; i++) {
+//         fact *= i;
+//     }
+//     return fact;
+// }
+// document.write("Factorial: " + factorial(5));
+
+
+// Q no: 06
+
+// function displayCounting(start, end) {
+//     for (let i = start; i <= end; i++) {
+//         document.write(i + "<br>");
+//     }
+// }
+// displayCounting(1, 10);
+
+
+// Q no: 07
+
+
+function square(num) {
+    return num * num;
+}
+document.write("Square: " + square(6));
+
